82ebbd9fcab69c6850b0e777300697cf4652c794
remiliance <56579705+remiliance@users.noreply.github.com>
2020-03-13 18:23:08 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO Product VALUES(1, 'Ordinateur portable' , 350, 120);
 INSERT INTO Product VALUES(2, 'Aspirateur Robot' , 500, 200);
 INSERT INTO Product VALUES(3, 'Table de Ping Pong' , 750, 400);
+INSERT INTO Product VALUES(4, 'Yoyo' , 750, 300);
